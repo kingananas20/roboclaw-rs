d9d7b41b8dbc5f60b07997cc19ff075091ee21ac
kingananas20 <jonasarter@outlook.com>
2025-03-30 22:02:30 +0200

    added write function for the connection struct

--- src/connection.rs
+++ src/connection.rs
@@ -1,8 +1,7 @@
 //! This module handles the serial connection between the controller and
 //! the RoboClaw.
 
-use std::error::Error;
-
+use crate::byte_operations::*;
 use crate::Commands;
 use crc16::{State, XMODEM};
 use serialport::{ClearBuffer, SerialPort};
@@ -10,26 +9,78 @@ use serialport::{ClearBuffer, SerialPort};
 pub struct Connection {
     port: Box<dyn SerialPort>,
     pub address: u8,
+    tries: u8,
     crc: State<XMODEM>,
+    buffer: Vec<u8>,
 }
 
 impl Connection {
-    pub fn new(port: Box<dyn SerialPort>, address: u8) -> Self {
-        let crc = State::<XMODEM>::new();
-        Connection { port, address, crc }
+    pub fn new(self, port: Box<dyn SerialPort>, address: u8, tries: u8) -> Self {
+        let crc = self.initialize_crc();
+        let buffer = Vec::new();
+        Connection {
+            port,
+            address,
+            tries,
+            crc,
+            buffer,
+        }
     }
 
-    fn initialize_crc(&mut self) -> State<XMODEM> {
+    /// initializes a new State for CRC16 XMODEM
+    fn initialize_crc(&self) -> State<XMODEM> {
         State::<XMODEM>::new()
     }
 
+    /// clears the input buffer of the connectio
     fn reset_connection(&mut self) {
-        let _ = self.port.clear(ClearBuffer::Input);
+        let _ = self.port.clear(ClearBuffer::All);
         self.crc = self.initialize_crc();
+        self.buffer.clear();
     }
 
+    /// sends the address and command and updates the crc
     fn send_command(&mut self, command: Commands) {
         self.crc.update(&[self.address, command as u8]);
-        let _ = self.port.write_all(&[self.address, command as u8]);
+        self.buffer.extend(&[self.address, command as u8]);
+    }
+
+    pub fn write(&mut self, command: Commands, values: &[u32]) -> bool {
+        for _ in 0..self.tries {
+            self.reset_connection();
+            self.send_command(command);
+
+            for &val in values {
+                match val {
+                    0..=0xFF => {
+                        self.crc.update(&[val as u8]);
+                        self.buffer.extend_from_slice(&[val as u8]);
+                    }
+                    0x100..=0xFFFF => {
+                        self.crc.update(&(val as u16).to_be_bytes());
+                        self.buffer.extend_from_slice(&(val as u16).to_be_bytes());
+                    }
+                    _ => {
+                        self.crc.update(&val.to_be_bytes());
+                        self.buffer.extend_from_slice(&val.to_be_bytes());
+                    }
+                }
+            }
+
+            let crc_bytes = self.crc.get().to_be_bytes();
+            self.buffer.extend_from_slice(&crc_bytes);
+
+            let _ = self.port.write_all(&self.buffer);
+
+            let mut ack = [0u8; 1];
+            let success: bool = match self.port.read_exact(&mut ack) {
+                Ok(_) => ack[0] == 0xFF,
+                Err(_) => false,
+            };
+
+            return success;
+        }
+
+        false
     }
 }
